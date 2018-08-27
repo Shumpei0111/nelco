009f95076930327b7c75aa7c58bf42f8005e58c7
shumpei otsubo <shumpei.o.7g@gmail.com>
2018-08-27 13:47:11 +0000

    mentor meeting, better toppages#index

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,6 +2,8 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @nelcopost = current_user.nelcopost.build
+      user_ids = current_user.followings.pluck(:id) + [current_user.id]
+      followings_users_posts = Nelcopost.where(user_ids).order("created_at DESC").page(params[:page])
       @nelcoposts = current_user.nelcopost.order("created_at DESC").page(params[:page])
     end
   end
