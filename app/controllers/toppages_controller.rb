29063243e3854c1aaacc298bbdec6e2eecb24135
shumpei otsubo <shumpei.o.7g@gmail.com>
2018-08-28 02:40:00 +0000

    toppages#index roop

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,9 +1,12 @@
 class ToppagesController < ApplicationController
+  
   def index
     if logged_in?
+      #debugger
+      @user = User.find_by(id: params[:id])
       @nelcopost = current_user.nelcopost.build
       user_ids = current_user.followings.pluck(:id) + [current_user.id]
-      followings_users_posts = Nelcopost.where(user_ids).order("created_at DESC").page(params[:page])
+      @followings_users_posts = Nelcopost.where(user_id: user_ids).order("created_at DESC").page(params[:page])
       @nelcoposts = current_user.nelcopost.order("created_at DESC").page(params[:page])
     end
   end
