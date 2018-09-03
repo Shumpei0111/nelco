77efc297b35d3f8d9cc5f147303f88dcf8e114e6
shumpei otsubo <shumpei.o.7g@gmail.com>
2018-09-03 13:17:07 +0000

    index image

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -6,6 +6,7 @@ class ToppagesController < ApplicationController
       @nelcopost = current_user.nelcopost.build
       user_ids = current_user.followings.pluck(:id) + [current_user.id]
       @followings_users_posts = Nelcopost.where(user_id: user_ids).order("created_at DESC").page(params[:page])
+      debugger
       @nelcopost_pages = current_user.nelcopost.order("created_at DESC").page(params[:page])
       @ranking_counts = Favorite.ranking
       @nelcoposts = Nelcopost.find(@ranking_counts.keys)
