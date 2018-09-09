7ca0c49c47b0f704bfb6a41be9f545467dd18d3a
shumpei otsubo <shumpei.o.7g@gmail.com>
2018-09-09 13:08:51 +0000

    mentor checked uploarder

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -10,8 +10,12 @@ class IconUploader < CarrierWave::Uploader::Base
     end
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
   # storage :fog
+  
+  unless Rails.env.production?
+    storage :file
+　end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
