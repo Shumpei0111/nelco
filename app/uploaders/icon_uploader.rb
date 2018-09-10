b001d1570d225ec3d2b9071823a918cbc9e00b12
shumpei otsubo <shumpei.o.7g@gmail.com>
2018-09-10 15:50:13 +0000

    uploader corrected

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -17,7 +17,7 @@ class IconUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  # storage :fog
+  #storage :fog
   
   #unless Rails.env.production?
   #  storage :file
